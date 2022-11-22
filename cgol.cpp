27a21b3c32feb8ed8b443c2029a316f38c12e0ab
ednielcampos <edniel_campos@uri.edu>
2022-11-22 12:51:28 -0500

    Create cgol files

---
+++ cgol.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Edniel Campos on 11/22/22.
+//
+
+#include "cgol.h"
---
+++ cgol.h
@@ -0,0 +1,8 @@
+//
+// Created by Edniel Campos on 11/22/22.
+//
+
+#ifndef FINAL_PROJ_CGOL_H
+#define FINAL_PROJ_CGOL_H
+
+#endif //FINAL_PROJ_CGOL_H
